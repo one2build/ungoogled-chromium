fd664e13dd52b918700e5b7a33efacc270fd23e7
Mark <me@markwylde.com>
2021-04-14 16:15:27 +1000

    Fix build.sh script

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
-docker build -t one2build/ungoogled-chromium .
+rm -rf output
+mkdir -p output
+
+# docker build -t one2build/ungoogled-chromium .
 docker run -v `pwd`/output:/output --net host one2build/ungoogled-chromium sh -c \
   "ls && cp -r /build/out/* /output && chown -R 1000:1000 /output"
 
 export ONE2BUILD_DIR=`pwd`
 
 cd output
+
 # Step 9) Package app
 ./pkg2appimage-*.AppImage ../Chrome.yml
 
@@ -14,6 +18,7 @@ rm -rf chrome-linux
 ./out/Chromium-*.AppImage --appimage-extract
 mv ./out/Chromium*.AppImage .
 rm -rf out
+rm -rf chromium
 mv ./squashfs-root ./chromium
 
 # Step 11) Create executable entrypoint
